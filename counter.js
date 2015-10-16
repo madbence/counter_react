0214c13c9c9acded836f0e73f9d7b5f4136da4da
Bence Dányi <bence@danyi.me>
2015-10-16 16:34:42 +0200

    Add redux

--- counter.js
+++ counter.js
@@ -1,23 +1,38 @@
 var React = require ('react');
 var ReactDOM = require ('react-dom');
+var redux = require ('redux');
+var reactRedux = require ('react-redux');
 
 var Counter = React.createClass({
-	getInitialState: function () {
-		return { count: 0 };	
-	},
-	handleClick: function () {
-		this.setState({ count: this.state.count + 1 });
-	},
-	render: function () {
-		return (
-			<div>
-				<h3>Counter using react</h3>
-				<h2>Number of clicks: {this.state.count}</h2>
-				<button onClick={this.handleClick}>Click me</button>
-			</div>	
-		); 
-	}
+  handleClick: function () {
+    this.props.dispatch({
+      type: 'INCREMENT',
+    });
+  },
+  render: function () {
+    return (
+      <div>
+        <h3>Counter using react</h3>
+        <h2>Number of clicks: {this.props.count}</h2>
+        <button onClick={this.handleClick}>Click me</button>
+      </div>
+    );
+  },
+});
+
+var WrappedCounter = reactRedux.connect(state => state)(Counter);
+
+var store = redux.createStore(function (state, action) {
+  switch (action.type) {
+    case 'INCREMENT': return { count: state.count };
+    default: return state;
+  }
+}, {
+  count: 0,
 });
 
 var mountNode = document.getElementById('reactCounterNode');
-ReactDOM.render(<Counter />, mountNode);
+ReactDOM.render(
+  <reactRedux.Provider store={store}>
+    <WrappedCounter />
+  </reactRedux.Provider>, mountNode);
